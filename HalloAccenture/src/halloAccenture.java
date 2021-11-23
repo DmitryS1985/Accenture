fab886239495a6cc388a68851d7dd7ec7f7d0211
Dmitrijs Sagandikovs <87562103+DmitryS1985@users.noreply.github.com>
2021-11-23 11:15:11 +0200

    halloAccenture

---
+++ HalloAccenture/src/halloAccenture.java
@@ -0,0 +1,13 @@
+public class halloAccenture {
+    public static void main(String[] args) {
+        byte first = 1;
+        short second = 2;
+        int third = 3;
+        long fourth = 4;
+        String five = "five";
+        boolean isSIx = true;
+        float seven = 7;
+        double eight = 8;
+        System.out.print("Numbers :" + first+ " " + second+ " " + third + " "+ fourth+ " " + five+ " " + isSIx+ " " + seven + " "+ eight);
+    }
+}
