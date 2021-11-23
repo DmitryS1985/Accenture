ed45141c3cb085bef8760c4e1dd8760112af19ff
Dmitrijs Sagandikovs <87562103+DmitryS1985@users.noreply.github.com>
2021-11-23 12:50:06 +0200

    Development : second commit - new Variable

--- HalloAccenture/src/halloAccenture.java
+++ HalloAccenture/src/halloAccenture.java
@@ -1,6 +1,7 @@
 public class halloAccenture {
     public static void main(String[] args) {
         int new_variable;
+        int secondVariable;
         byte first = 1;
         short second = 2;
         int third = 3;
